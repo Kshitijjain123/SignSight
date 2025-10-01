319852271ad4929ac0392825b85e3721cb969418
Kshitij Jain <kshitijj2006@gmail.com>
2025-10-02 01:42:24 +0530

    Add files via upload

---
+++ flaskapp.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "SignSight API is running!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Dummy response for now
+    data = request.get_json()
+    return jsonify({"prediction": "Hello", "input": data})
+
+if __name__ == '__main__':
+    app.run(debug=True)
