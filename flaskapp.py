aa9250fd24bc200c95d1efbc7acd3710e3a5b3c2
Kshitij Jain <kshitijj2006@gmail.com>
2025-10-02 02:23:21 +0530

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,7 +1,9 @@
-#redeploy
 from flask import Flask, request, jsonify
+from tensorflow.keras.models import load_model
+import numpy as np
 
 app = Flask(__name__)
+model = load_model("signSight_model.h5")  # Make sure this file is in your repo
 
 @app.route('/')
 def home():
@@ -9,10 +11,12 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Dummy response for now
     data = request.get_json()
-    return jsonify({"prediction": "Hello", "input": data})
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
+    
+    # Example: convert input to numpy array
+    input_array = np.array(data["input"]).reshape(1, -1)  # Adjust shape as needed
+    
+    prediction = model.predict(input_array)
+    predicted_class = int(np.argmax(prediction, axis=1)[0])  # For classification models
+    
+    return jsonify({"prediction": predicted_class})
